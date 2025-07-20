31f357dd03f62b3359b524b4ddd8ebf8981ec933
elorm <jiffaelorm@gmail.com>
2025-07-20 21:43:58 +0000

    django views..function and class based

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # Create your views here.
+#function based view
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books':books}
+
+
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+#class based view
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['books'] = Book.objects.filter(library=library)
+        return context
+
