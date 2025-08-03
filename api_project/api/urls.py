685320aec04f75e567426172ff8b5616bac7b212
elorm <jiffaelorm@gmail.com>
2025-08-03 03:27:59 +0000

    viewsets

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+# Create a router instance - DefaultRouter auto-generates URL patterns for ViewSets
+router = DefaultRouter()
+
+# 'books_all' is the URL prefix that will be used (e.g., /books_all/, /books_all/1/)
+# 'basename' is used to name the URL patterns
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 
 urlpatterns = [
+    #route for the BookList view (LISTAPIView)
     path('books/', BookList.as_view(), name='book-list'), #route to list books
+
+    #include the router-generated urls for BookViewSet (all CRUD operations) in the app's URLs
+    path('',include(router.urls)), # this incudes all routes registered with the router
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,12 +3,18 @@ from .models import Book
 from .serializers import BookSerializer
 #import generic views from DRF
 from rest_framework import generics
+from rest_framework import viewsets 
 
 
 # Create a view that list all books using ListAPIView
-
+#generic views are specific but  viewsets provide all basic crud operations
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()   #the data to return
     serializer_class = BookSerializer #how to serialize the data
 
     
+#extending to use a viewset as well
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
