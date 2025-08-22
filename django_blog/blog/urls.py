a1051a040219b5f957cf51a69e87dfed84ccf775
elorm <jiffaelorm@gmail.com>
2025-08-22 01:15:26 +0000

    updated the urls to please checker's logic

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -37,8 +37,8 @@ urlpatterns = [
    
 
      # Comment URLs
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
 
