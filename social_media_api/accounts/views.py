66927b4f18d5c31d05548cad2873f87f7c90c5cb
elorm <jiffaelorm@gmail.com>
2025-08-24 23:37:27 +0000

    checker issues

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -37,24 +37,27 @@ class UserDetailView(APIView):
 # accounts/views.py
 
 
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         user_to_follow = get_object_or_404(CustomUser, id=user_id)
-
         if user_to_follow == request.user:
-            return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.add(user_to_follow)
-        return Response({"message": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
+        return Response({"detail": f"You are now following {user_to_follow.email}."}, status=status.HTTP_200_OK)
 
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        if user_to_unfollow == request.user:
+            return Response({"detail": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.remove(user_to_unfollow)
-        return Response({"message": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
+        return Response({"detail": f"You have unfollowed {user_to_unfollow.email}."}, status=status.HTTP_200_OK)
