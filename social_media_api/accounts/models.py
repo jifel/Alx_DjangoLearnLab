a36322d71113336cef5706243f43f15c23cb59e0
elorm <jiffaelorm@gmail.com>
2025-08-24 22:34:41 +0000

    Project Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    
+    # self-referential many-to-many for followers
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  # username still required, but login uses email
+
+    def __str__(self):
+        return self.email
